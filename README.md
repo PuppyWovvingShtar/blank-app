35fb2d7604f2a1fd1dea820fa9737a8d9a18d32f
PuppyWovvingShtar <elizabeth.spirydovich@gmail.com>
2024-09-23 20:44:37 +0000

    thank you

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 🎈 Blank app template
+# 🌺 Blank app template
 
 A simple Streamlit app template for you to modify!
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,17 @@
+
+
+
+
 import streamlit as st
 
 st.title("🌺🤪 Zergei's Programming club")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
+st.checkbox ("Take a deep breath")
+
+st.checkbox("Smile")
+
+if st.button("click this button"):
+    st.balloons()
+    st.write("🎉Happy Birthday to PAPA. Happy Birthday toooooo PAPA. Happy Birthday Happy Birthday Happy Birthday to yoooooouuuuuuuuuuuuuuuuuuuuuuuuuu!🎉")
+    st.video("https://www.youtube.com/watch?v=F9yPoIsDsUg&list=PLfPhisoXl8jtjLi5AjpZgv9iaXeXbpbxo&index=10")
+    st.video("https://www.youtube.com/watch?v=8aGtX5knSNc")
